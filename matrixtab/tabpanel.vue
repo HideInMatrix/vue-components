85550a9066e4ebd59838f88383eed2570a67d104
David <ezioauditorebaobao@gmail.com>
2021-07-01 14:58:37 +0800

    Tab

---
+++ matrixtab/tabpanel.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: David
+ * @Date: 2021-01-30 10:51:23
+ * @LastEditTime: 2021-01-30 10:51:35
+ * @LastEditors: David
+ * @Description: 
+ * @FilePath: /WebFront/src/components/matrixtab/tabpanel.vue
+ * @可以输入预定的版权声明、个性签名、空行等
+-->
+<template>
+  <div class="nut-tab-panel hide">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "nut-tab-panel",
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  mounted() {},
+  methods: {}
+};
+</script>
+<style lang="scss"></style>
